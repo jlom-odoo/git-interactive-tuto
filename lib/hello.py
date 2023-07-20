582b47310f812d6f27afcb48e656bf11ff374875
JLOM <jlom@odoo.com>
2023-07-20 09:26:49 -0600

    Updated to use Greeter class

--- lib/hello.py
+++ lib/hello.py
@@ -1,6 +1,7 @@
 # Author: JLOM (jlom@odoo.com)
 
 import sys
+from greeter import Greeter
 
 # When no name provided, default is Luis
 user_name = "Luis"
@@ -8,4 +9,5 @@ user_name = "Luis"
 if len(sys.argv) == 2:
     user_name = sys.argv[1]
 
-print("Hello,", user_name)
+greeter = Greeter(user_name)
+greeter.greet()
